00a8f16cccf24b5c523500791ba11109e0aeb94a
Omar Ortiz <oortiz.mcp@outlook.com>
2020-05-21 23:00:29 -0500

    Added Script for buiding AZ custom policies

---
+++ Creating a Custom policy.ps1
@@ -0,0 +1,9 @@
+﻿# Create a policy with Visual Studio code and deploy template
+$cred = get-credential
+$rg= Get-AzResourceGroup -Name RoleAssignment-rg
+$subscription = Get-AzSubscription -SubscriptionName 'yoursubnamehere' 
+Connect-AzAccount -Credential $cred -Tenant $subscription.TenantId
+
+$policy = New-AzPolicyDefinition -Name "Name of your policy" -policy "C:\path\buildingpolicy.json"
+
+New-AzPolicyAssignment -Name "Name of your policy assignment" -PolicyDefinition $policy -Scope $rg.ResourceId -location 'centralus'
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Powershell Scripts for Simple deployments
 
 In this repo, I share my most common and simple powershell scripts
 
-#Note: These are not intended to use in large scale Production environments. The purpose intended is to have some simple lines of code handy when needed.
+##Note: These are not intended to use in large scale Production environments. The purpose intended is to have some simple lines of code handy when needed.
