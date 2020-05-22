d26291a708e0b3a26e8c14e98e814d0f75a44163
Omar Ortiz <oortiz.mcp@outlook.com>
2020-05-21 23:04:11 -0500

    Added how to deploy ARM templates

---
+++ deploying-ARM-templates.ps1
@@ -0,0 +1,13 @@
+﻿#Deploying ARM templates
+
+#Create variables
+$rg = New-AzResourceGroup -Name ARM-rg -Location 'East US 2'
+$template = "C:\edityourpath\template.json"
+$parameter = "C:\edityourpath\parameters.json"
+
+#Deploy template
+New-AzResourceGroupDeployment `
+-Name deployment1 `
+-ResourceGroupName ARM-RG `
+-TemplateFile $template `
+-TemplateParameterFile $parameter
