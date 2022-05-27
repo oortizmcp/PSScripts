927cd7e7daa59f54b2c558bc91e6d473bb110932
Omar Ortiz <53305878+oortizmcp@users.noreply.github.com>
2022-05-27 12:58:38 -0500

    Update Enable-Replication.ps1

--- Enable-Replication.ps1
+++ Enable-Replication.ps1
@@ -309,9 +309,9 @@ $DeploymentScriptOutputs['RecoveryProtectionContainerMapping'] = $reverseContain
 $enableReplicationJobs = New-Object System.Collections.ArrayList
 foreach ($sourceVmArmId in $sourceVmARMIds) {
 	# Trigger Enable protection
-	$vmIdTokens = $sourceVmArmId.Split('/');
-	$vmName = $vmIdTokens[8].Split('''}');
-	$vmResourceGroupName = $vmIdTokens[4]
+	$vmIdTokens = $sourceVmArmId.Split('/').Split("'}");
+	$vmName = $vmIdTokens[8] 
+    	$vmResourceGroupName = $vmIdTokens[4]
 	$message = 'Enable protection to be triggered for {0} using VM name {1} as protected item ARM name.' -f $sourceVmArmId, $vmName
 	$vm = Get-AzVM -ResourceGroupName $vmResourceGroupName -Name $vmName
 	Write-Output $message
